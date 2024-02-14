a004116e0043d0191a83d5bc36d6f22b9c6538a0
Mohamad Abou Salem <mba26@mail.aub.edu>
2024-02-14 15:06:23 +0200

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,32 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'valentine';
+
+  yesClicked = false;
+
+  mouseTarget = document.querySelector('#mouseTarget');
+
+  onMouseOver() {
+    const button = document.getElementById('nobtn');
+    button?.addEventListener('mouseover', function () {
+      button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+      button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+    });
+  }
+
+  clickNo() {
+    const button = document.getElementById('nobtn');
+    button?.addEventListener('click', function () {
+      button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+      button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+    });
+  }
+
+  clickYes() {
+    this.yesClicked = true;
+  }
 }
